8eda91f08f8f8c876709b68c16f86400b733f038
Doctor <thirvondukr@gmail.com>
2022-08-25 02:32:00 +0300

    Rewrite commit middleware as ASGI middleware

--- src/middleware.py
+++ src/middleware.py
@@ -1,14 +1,28 @@
-from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
-from starlette.requests import Request
-from starlette.responses import Response
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.types import ASGIApp, Message, Receive, Scope, Send
 
 
-class CommitSessionMiddleware(BaseHTTPMiddleware):
-    async def dispatch(
-        self, request: Request, call_next: RequestResponseEndpoint
-    ) -> Response:
-        response = await call_next(request)
-        if hasattr(request.state, "sqlalchemy_session"):
-            await request.state.sqlalchemy_session.commit()
+class CommitSessionMiddleware:
+    def __init__(
+        self,
+        app: ASGIApp,
+    ) -> None:
+        self.app = app
 
-        return response
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        if scope["type"] != "http":
+            await self.app(scope, receive, send)
+            return
+
+        async def _inner(message: Message) -> None:
+            if message["type"] != "http.response.start":
+                await send(message)
+                return
+            if "state" in scope and (
+                session := scope["state"].get("sqlalchemy_session")
+            ):
+                if session.in_transaction():
+                    await session.commit()
+            await send(message)
+
+        await self.app(scope, receive, _inner)
