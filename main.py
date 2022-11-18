af03c796030f0ec342fb877ddcaba9ed3ff9d3cb
Doctor <thirvondukr@gmail.com>
2022-11-18 18:56:42 +0300

    Fix path to application in main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import uvicorn
 
 if __name__ == "__main__":
     dotenv.load_dotenv(".env")
-    uvicorn.run("app:create_app", factory=True, reload=True)
+    uvicorn.run("api.app:create_app", factory=True, reload=True)
