4edd4db2fbdac0fc3312ab3d4172db94821f3d76
Doctor <thirvondukr@gmail.com>
2023-03-14 09:30:16 +0300

    Format code

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -1,13 +1,13 @@
 import asyncio
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import engine_from_config, pool
 from sqlalchemy.engine import Connectable, Connection
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 import db.models  # noqa: F401
 import sentry
+from alembic import context
 from db import Base
 from settings import DatabaseSettings, get_settings
 
