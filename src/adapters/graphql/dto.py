2cf4a07d9e57c79b45335d8f5f01877db970572a
Doctor <thirvondukr@gmail.com>
2023-07-27 08:18:28 +0300

    Add `_contra` suffix to typevar

--- src/adapters/graphql/dto.py
+++ src/adapters/graphql/dto.py
@@ -2,31 +2,31 @@ import abc
 from collections.abc import Iterable
 from typing import Generic, Protocol, Self, TypeVar
 
-_TModel = TypeVar("_TModel", contravariant=True)
+_TModel_contra = TypeVar("_TModel_contra", contravariant=True)
 _TType = TypeVar("_TType")
 
 
-class DTOMixinProtocol(Protocol[_TModel, _TType]):
+class DTOMixinProtocol(Protocol[_TModel_contra, _TType]):
     @classmethod
-    def from_orm_list(cls, models: Iterable[_TModel]) -> list[_TType]:
+    def from_orm_list(cls, models: Iterable[_TModel_contra]) -> list[_TType]:
         ...  # pragma: no coverage
 
 
-class DTOMixin(Generic[_TModel]):
+class DTOMixin(Generic[_TModel_contra]):
     @classmethod
     @abc.abstractmethod
-    def from_orm(cls, model: _TModel) -> Self:
+    def from_orm(cls, model: _TModel_contra) -> Self:
         raise NotImplementedError
 
     @classmethod
     def from_orm_optional(
         cls,
-        model: _TModel | None,
+        model: _TModel_contra | None,
     ) -> Self | None:  # pragma: no cover
         if model is None:
             return None
         return cls.from_orm(model)
 
     @classmethod
-    def from_orm_list(cls, models: Iterable[_TModel]) -> list[Self]:
+    def from_orm_list(cls, models: Iterable[_TModel_contra]) -> list[Self]:
         return [cls.from_orm(model) for model in models]  # pragma: no cover
